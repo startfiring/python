8ced06c7b24e8aa0a7eb5300caff9b7490d6fb56
startfiring <Kang_zian@hotmail.com>
2019-02-09 14:00:21 +0800

    2019 2-9

---
+++ basic/function.py
@@ -0,0 +1,6 @@
+
+def say_hi(name , age):
+    print("Hello " + name + ". You are " + str(age))
+
+say_hi("John" , 14)
+say_hi("Jenny" , 20)
---
+++ basic/tuple.py
@@ -0,0 +1,8 @@
+
+coordinates = [(4, 5), (6,7),(80, 34)]
+coordinates[1] = 10
+print(coordinates[1])
+
+print(coordinates)
+
+
