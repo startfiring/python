c3a0966fdd3da4c0aeca1d4204cb432fea86df6f
startfiring <Kang_zian@hotmail.com>
2019-02-10 13:46:00 +0800

    2019 2-10

---
+++ basic/if_statements.py
@@ -0,0 +1,34 @@
+
+is_male = False
+is_tall = True
+
+if is_male or is_tall:
+    print("You are a male or tall or both")
+else:
+    print("You are neither male nor tall")
+
+if is_male and is_tall:
+    print("You are a tall male")
+else:
+    print("You are either not male or not tall or both")
+
+if is_male and is_tall:
+    print("You are a tall male")
+elif is_male and not(is_tall):
+    print("You are a short male")
+elif not(is_male) and is_tall:
+    print("You are not a male, but are tall")
+else:
+    print("You are either not male or not tall or both")
+
+
+
+def max_num(num1, num2,num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+print(max_num(3,4,5))
---
+++ basic/return_statement.py
@@ -0,0 +1,6 @@
+
+def cube(num):
+    return num*num*num
+
+result = cube(8)
+print(result)
