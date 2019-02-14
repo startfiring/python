7ed537c4a222af4e39976c0d27c6e95e7b7eb678
startfiring <Kang_zian@hotmail.com>
2019-02-14 22:34:33 +0800

    2019 2-14

---
+++ basic/exponent_function.py
@@ -0,0 +1,29 @@
+
+def raise_to_power1(base_num, pow_num):
+
+    if (base_num == 0) and (pow_num <= 0):
+        return ("Math ERROR")
+    
+    count = 1
+    result = base_num
+    while count < pow_num:
+        result = result * base_num
+        count += 1
+    return result
+
+
+def raise_to_power2(base_num, pow_num):
+
+    if (base_num == 0) and (pow_num <= 0):
+        return ("Math ERROR")
+
+    result = 1
+    for index in range(pow_num):
+        result = result * base_num
+
+    return result
+print(raise_to_power1(3, 5))
+print(raise_to_power2(3, 5))
+
+
+
---
+++ basic/for_loop.py
@@ -0,0 +1,20 @@
+
+for letter in "John and Jenny":
+    print(letter)
+
+friends = ["Jim","Kerry","Kevin"]
+for friend in friends:
+    print(friend)
+
+for index in range(3,10):
+    print(index)
+
+friends = ["Jim","Kerry","Kevin"]
+for index in range(len(friends)):
+    print(friends[index])
+
+for index in range(5):
+    if index == 0:
+        print("first Iteration")
+    else:
+        print("not first")
